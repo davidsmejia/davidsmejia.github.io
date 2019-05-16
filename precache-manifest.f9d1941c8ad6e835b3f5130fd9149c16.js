6cb83677cce9cc1e4c2b4e9833b601f86a616955
David Mejia <davidsmejia@gmail.com>
2019-05-16 13:54:55 -0400

    Updates

---
+++ precache-manifest.f9d1941c8ad6e835b3f5130fd9149c16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2f4cce095262c8d82d2d5380109f83b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbdeb85283ca52bf742c",
+    "url": "/static/css/main.eb70b68b.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "bbdeb85283ca52bf742c",
+    "url": "/static/js/main.dbc40652.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
