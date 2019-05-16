f2a3dd1dcfdacbc5a3b1e165f19b3550c3862924
David Mejia <davidsmejia@gmail.com>
2019-05-16 13:14:52 -0400

    last things first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import me from './me.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={me} className="App-logo" alt="logo" />
+        <p>David S Mejia</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
